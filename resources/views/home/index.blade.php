0904ed7fbbc88a418e7909a60e805e72df65f425
Mahendra-uk <mahendrakhadake100@gmail.com>
2025-08-26 17:50:56 +0530

    ecom-store-launch

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    @include('home.head')
+</head>
+
+<body>
+    <!-- hero area starts -->
+    <div class="hero_area">
+        <!-- header section starts -->
+
+        @include('home.header')
+
+        <!-- end header section -->
+
+
+        <!-- slider section starts -->
+
+        @include('home.slider')
+
+        <!-- end slider section -->
+    </div>
+    <!-- end hero area -->
+
+    <!-- shop section starts -->
+
+    @include('home.products')
+
+    <!-- end shop section -->
+
+
+    <!-- contact section starts -->
+
+    @include('home.contact')
+
+    <!-- end contact section -->
+
+
+
+    <!-- footer section starts -->
+
+    @include('home.footer')
+
+    <!-- end footer section -->
+</body>
+
+</html>
